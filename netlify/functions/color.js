d2fe276e6440f06ce7218d3cf9676715e029f7ca
Daniel Woelfel <dwwoelfel@gmail.com>
2022-02-25 11:38:45 -0800

    add a ttl

--- netlify/functions/color.js
+++ netlify/functions/color.js
@@ -2,11 +2,11 @@ const { builder } = require("@netlify/functions");
 const NetlifyGraph = require("./netlifyGraph");
 
 async function handler(event) {
-  const [,_prefix, ...packageNameParts] = event.path.split("/");
+  const [, _prefix, ...packageNameParts] = event.path.split("/");
 
   const name = packageNameParts.join("/");
 
-  console.log('name', name, _prefix, event.path);
+  console.log("name", name, _prefix, event.path);
 
   const time = new Date().toString();
   console.log(`Time: ${time}`);
@@ -20,6 +20,7 @@ async function handler(event) {
     headers: {
       "Content-Type": "application/json",
     },
+    ttl: 3600,
     body: JSON.stringify(errors ? { errors } : { data }, null, 2),
   };
 }
